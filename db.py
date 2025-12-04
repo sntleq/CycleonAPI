5f1a77edae78eec346ff42eb4bb4fb90100ec3fe
Jodeci <joedpacibe@gmail.com>
2025-12-04 16:15:35 +0800

    added db variable

--- db.py
+++ db.py
@@ -1,6 +1,17 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from contextlib import contextmanager
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+@contextmanager
+def get_db():
+    conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+    try:
+        yield conn
+    finally:
+        conn.close()
 
 DB_CONFIG = {
     "host": "localhost",
